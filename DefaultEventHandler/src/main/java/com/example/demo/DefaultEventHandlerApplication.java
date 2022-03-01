22d9571219b3ba660e4edf8664c0794115643c4a
niranjana <67066151+niranjana-kamalapuram@users.noreply.github.com>
2022-03-01 10:52:58 +0530

    Add files via upload

---
+++ DefaultEventHandler/src/main/java/com/example/demo/DefaultEventHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultEventHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultEventHandlerApplication.class, args);
+	}
+
+}
